c2d931053f4078e0a747c27afecfed6bd4f7fc5c
abhinev08 <abhinev.kasba@globtier.in>
2023-10-12 23:09:16 +0530

    added hero section with latest news topic

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ import Union from "../src/assets/Union.png";
 
 function App() {
   const [data, setData] = useState([]);
-  const [data2, setData2] = useState([]);
 
   const apiKey = "a7f52ce8b11242b3aa32bec357483a32";
-  const apiKey2 = "api_key_2";
 
   const apiUrl = `https://newsapi.org/v2/everything?q=apple&from=2023-10-11&to=2023-10-11&sortBy=popularity&apiKey=${apiKey}&language=en`;
-  const apiUrl2 = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey2}`;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,25 +27,6 @@ function App() {
 
     fetchData();
   }, [apiUrl]);
-
-  useEffect(() => {
-    const fetchData2 = async () => {
-      try {
-        const response = await fetch(apiUrl2);
-
-        if (!response.ok) {
-          throw Error("Network response was not ok");
-        }
-
-        const jsonData = await response.json();
-        setData2(jsonData.articles);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData2();
-  }, [apiUrl2]);
 
   return (
     <div className="container-fluid" style={{ background: "#FDFDFD" }}>
@@ -70,28 +48,42 @@ function App() {
         </div>
       </nav>
 
-      <div className="container" style={{ marginTop: "150px" }}>
+      <div className="container" style={{ marginTop: "120px" }}>
         <div className="hero-section">
           <h2 className="hero-heading">Hot Topics</h2>
           <div className="row mt-4">
-            {data2.map((item:any, index:any) => (
-              <div className="col-md-3" key={index}>
-                <div className="card">
+            {data.slice(0, 1).map((item: any, index) => (
+              <div className="col-md-8 col-12" key={index}>
+                <div className="card hero" style={{ border: "transparent" }}>
                   {item.urlToImage && (
-                    <img
-                      className="card-img-top"
-                      src={item.urlToImage}
-                      alt={item.title}
-                      style={{ height: "200px", width: "100%" }}
-                    />
+                    <div
+                      className="image-overlay"
+                      style={{
+                        backgroundImage: `url(${item.urlToImage})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        padding: "5px 10px",
+                      }}
+                    >
+                      <div className="overlay-text">
+                        <h5 className="card-title text-white">{item.title}</h5>
+                        <div className="d-flex justify-content-between align-items-center mt-3">
+                          <p className="published-at mb-2">
+                            {new Date(item.publishedAt).toLocaleString()}
+                          </p>
+                          <p className="author mb-2">{item.author}</p>
+                        </div>
+                      </div>
+                    </div>
                   )}
-                  <div className="card-body">
-                    <h5 className="card-title">{item.title}</h5>
-                    <p className="published-at">
-                      Published at:{" "}
-                      {new Date(item.publishedAt).toLocaleString()}
-                    </p>
-                    <p>{item.description}</p>
+                </div>
+              </div>
+            ))}
+            {data.slice(0, 1).map((item: any, index) => (
+              <div className="col-md-4 col-12 mt-4" key={index}>
+                <div className="card" style={{ border: "transparent" }}>
+                  <div className="card-body hot-topic">
+                    <p>{item.content}</p>
                   </div>
                 </div>
               </div>
@@ -102,8 +94,8 @@ function App() {
         <div className="section mt-4">
           <h4 className="section-heading">Latest News</h4>
           <div className="row mt-4">
-            {data.map((item: any, index: any) => (
-              <div className="col-md-3 mb-5" key={index}>
+            {data.slice(2).map((item: any, index) => (
+              <div className="col-md-3 col-12 mb-5" key={index}>
                 <div className="card" style={{ border: "transparent" }}>
                   {item.urlToImage && (
                     <img
@@ -116,10 +108,9 @@ function App() {
                   <div className="card-body">
                     <h5 className="card-title">{item.title}</h5>
                     <div className="mt-5">
-                    <p className="published-at">
-                      Published at:{" "}
-                      {new Date(item.publishedAt).toLocaleString()}
-                    </p>
+                      <p className="published-at">
+                        {new Date(item.publishedAt).toLocaleString()}
+                      </p>
                     </div>
                   </div>
                 </div>
